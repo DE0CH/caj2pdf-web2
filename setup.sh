c9d023a2d6ecf2ad8d6d002cefe64372a9873c12
Deyao Chen <chendeyao000@gmail.com>
2023-12-28 15:56:28 +0800

    update scripts

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@ sudo apt-get install -y libpoppler-private-dev
 scripts/clone.sh caj2pdf https://github.com/caj2pdf/caj2pdf.git e7bb0bfd43edb8ce29ea02771f3a1850ddef550d
 cd caj2pdf/lib
 cc -Wall -fPIC --shared -o libjbigdec.so jbigdec.cc JBigDecode.cc
-cc -I /usr/include/poppler -Wall -fPIC -shared -o libjbig2codec.so decode_jbig2data.cc -lpoppler
+cc -std=c++17 -I /usr/include/poppler -Wall -fPIC -shared -o libjbig2codec.so decode_jbig2data.cc -lpoppler
 cd ..
 mv lib/libjbigdec.so .
 mv lib/libjbig2codec.so .
+python3 -m venv venv
+source venv/bin/activate
 pip3 install -r requirements.txt
--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
+source caj2pdf/venv/bin/activate
 cd web
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 rm index.mjs; npx tsc && mv index.js index.mjs && node index.mjs | ts | tee -a ../log.txt
